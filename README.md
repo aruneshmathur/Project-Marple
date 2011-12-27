74d20ce1f7b278255abb051a6368b2c5b16a48ff
Arunesh Mathur <aruneshmathur1990@gmail.com>
2011-12-27 16:20:10 +0530

    file changes; functions will appear in one file

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ An empirical study of code reuse in open source projects.
 
 Switched to Python in this branch, primarily to explore the
 possibility of using database to store data. The other branch
-will continue to exist though. 
+will continue to exist.
---
+++ format-hash-winnow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+def stripchars(s, chars):
+    return s.translate(None, chars)
+
+def generate_hashes(string, k, B = 137, M = 5463458053):
+    hash_list = []
+
+    length = len(string)
+    count = length - k + 1
+
+    if count <= 0:
+        return None 
+
+    t = 0
+    j = 1
+
+    # Calculate ((B ^ k) - 1) once
+    for i in range(0, k - 1):
+        j = (j * B) % M
+    
+    for i in range(0, k):
+        t = ((t * B) + ord(string[i])) % M
+
+    hash_list.append(t)
+
+    for i in range(1, count):
+        t = (hash_list[i-1] - (ord(string[i-1]) * j)) % M
+        t = (t * B) % M
+        t = (t + ord(string[i+k-1])) % M
+        hash_list.append(t)
+
+    return hash_list
+
+if __name__ == "__main__":
+
+    print stripchars("printf(\"Hello there\");", "\"(); ");
+
+    for r in generate_hashes("Hello there madam!", 5):
+        print r
