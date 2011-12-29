ff8959a0cb2d5b315f66524067086554901beca6
Arunesh Mathur <aruneshmathur1990@gmail.com>
2011-12-30 04:08:30 +0530

    winnow fixed to obey new structures

--- format_hash_winnow.py
+++ format_hash_winnow.py
@@ -28,12 +28,13 @@ def gen_hash_list(string, file_numbers, k):
     hash_list.append([t,unique_elements(file_numbers[0:k])])
 
     for i in range(1, count):
-        t = (hash_list[i-1] - (ord(string[i-1]) * j)) % M
+        t = (hash_list[i-1][0] - (ord(string[i-1]) * j)) % M
         t = (t * B) % M
         t = (t + ord(string[i+k-1])) % M
         hash_list.append([t,unique_elements(file_numbers[i:i+k])] )
 
     return hash_list
+
 
 def hash_lines(lines_with_no, k = 5):
     file_string = ''
@@ -45,11 +46,11 @@ def hash_lines(lines_with_no, k = 5):
     return gen_hash_list(file_string, file_numbers, k)
 
 
-
 def record(hash_list, i):
-    return (hash_list[i], i)
+    return (hash_list[i][0], hash_list[i][1], i)
 
-def winnow(hash_list=[], w = 4):
+
+def winnow(hash_list, w = 4):
 
     winnow_list = []
     hash_list_length = len(hash_list)
@@ -59,14 +60,16 @@ def winnow(hash_list=[], w = 4):
     if not param < w:
         param = w
 
-    global_min = M + 1  
+    global_min = M + 1 
+    line_list = []
         
     for i in range(param - 1, -1, -1):
-        if hash_list[i] < global_min:
-            global_min = hash_list[i]
+        if hash_list[i][0] < global_min:
+            global_min = hash_list[i][0]
+            line_list = hash_list[i][1]
             index = i
 
-    winnow_list.append(global_min)
+    winnow_list.append([global_min, line_list])
 
     if hash_list_length < w:
         return winnow_list
@@ -76,15 +79,15 @@ def winnow(hash_list=[], w = 4):
             global_min = M + 1
 
             for j in range(w + i - 1, i - 1, -1):
-                if hash_list[j] < global_min:
-                    (global_min, index) = record(hash_list, j)
+                if hash_list[j][0] < global_min:
+                    (global_min, line_list, index) = record(hash_list, j)
 
-            winnow_list.append(global_min)
+            winnow_list.append([global_min, line_list])
 
         else:
-            if hash_list[w + i - 1] < global_min:
-                (global_min, index) = record(hash_list, w + i - 1)
-                winnow_list.append(global_min)
+            if hash_list[w + i - 1][0] < global_min:
+                (global_min, line_list, index) = record(hash_list, w + i - 1)
+                winnow_list.append([global_min, line_list])
 
 
     return winnow_list
