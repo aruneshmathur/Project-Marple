bce08982567baa9e70409ce3fb0f7b451cf000bb
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-01-04 00:00:09 +0530

    database integrated

--- file_input.py
+++ file_input.py
@@ -2,12 +2,17 @@
 
 from format_hash_winnow import hash_lines, winnow
 import utils, sys
+import database
 
 name = "filename"
 content = "content"
 chars = " \'\";()#\n{}"
 
 def process_files(files_list):
+
+    db = database.WinnowDB()
+    db.clear()
+    db.setup()
 
     for f in files_list:
         holder = open(f, 'r')
@@ -24,11 +29,12 @@ def process_files(files_list):
         hash_list = hash_lines(lines)
         winnow_list = winnow(hash_list, 12)
 
-        for win in winnow_list:
-            sys.stdout.write(str(win[0]) + " in lines " + str(win[1]) + "\n")
+        #for win in winnow_list:
+            #sys.stdout.write(str(win[0]) + " in lines " + str(win[1]) + "\n")
 
-        #record_into_database(winnow_list)
-    
+        db.insert_file_hash(f, winnow_list)
+        
+    db.close()
 
 if __name__ == '__main__':
-    process_files(['a.txt']);
+    process_files(['large.txt']);
