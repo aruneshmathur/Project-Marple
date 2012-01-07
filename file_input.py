f624a8f0fd4583201025479056694d3bc204b138
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-01-07 17:24:44 +0530

    line numbers in result

--- file_input.py
+++ file_input.py
@@ -35,29 +35,35 @@ def process_files(files_list):
         db.insert_file_hash(f, winnow_list)
 
 
-    final_similarity_list = {}
+    final_similarity_dict = {}
     
     for f in files_list:
         hash_list = db.get_hashes(f)
         similar_to_this_file = {}
+        similar_to_this_file_line_no = {}
 
         for h in hash_list:
-            similar_file_list = db.get_filenames(h[0])
+            similar_file_list = db.get_filenames(h[0], f)
 
             for sim in similar_file_list:
                 similar_to_this_file[sim[0]] = similar_to_this_file.get(sim[0],0) + 1
+                
+                l = similar_to_this_file_line_no.get(sim[0], [])
+                l.append(sim[1])
+                similar_to_this_file_line_no[sim[0]] = l
 
-
-        final_similarity_list[f] = [x for x in similar_to_this_file.keys() 
+        final_similarity_dict[f] = [[x,
+                                     utils.unique_elements(similar_to_this_file_line_no[x])] for x in similar_to_this_file.keys() 
                                     if similar_to_this_file[x] > threshold]     
 
      
-    for k in final_similarity_list.keys():
-        print k, final_similarity_list[k]
+    for k in final_similarity_dict.keys():
+        print k, final_similarity_dict[k]
 
     db.close()
 
 
 
 if __name__ == '__main__':
-    process_files(['a.txt']);
+    process_files(['test/a.txt', 'test/b.txt', 'test/c.txt', 'test/d.txt',
+                   'test/e.txt']);
