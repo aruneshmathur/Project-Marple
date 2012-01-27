3faddd68de9dd6723f23879896761e329cebe7f8
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-01-28 01:16:45 +0530

    added list files function

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import os
+
 def stripchars(s, chars):
     return s.translate(None, chars)
 
@@ -10,3 +12,17 @@ def unique_elements(ele_list):
         dic[ele] = 1
 
     return dic.keys()
+
+def list_files(path, dest_path):
+    f = open(dest_path, 'a')
+    get_file_list_path(path, f)
+    f.close()
+
+def get_file_list_path(path, dest_file):    
+    for f in os.listdir(path):
+        if os.path.isdir(path + '/' + f) == True:
+            get_file_list_path(path + '/' + f, dest_file)
+        else:
+            dest_file.write(path + '/' + f + '\n')
+
+
