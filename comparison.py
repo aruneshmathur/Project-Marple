ad993559d809daf5ad28b379ec6c67f6f1f694bd
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-02-06 14:07:05 +0530

    line numbers

--- comparison.py
+++ comparison.py
@@ -4,6 +4,9 @@ import string, utils
 
 text = "TEXT"
 line_no = "LINE_NO"
+lines1 = "LINES1"
+lines2 = "LINES2"
+
 threshold = 10
 
 
@@ -33,20 +36,29 @@ def LCS(text_dict1, text_dict2, min_length):
             else:
                 c[i][j] = max(c[i-1][j], c[i][j-1])
 
-    return LCS_string(c, str1, str2, min_length)
+    return LCS_string(c, text_dict1, text_dict2, min_length)
 
 
-def LCS_string(c, str1, str2, min_length):
+def LCS_string(c, text_dict1, text_dict2, min_length):
+
+    str1 = text_dict1[text]
+    str2 = text_dict2[text]
 
     i = len(str1) 
     j = len(str2)
 
-    res = ""
-    cur_str = ""
+    str_lines1 = text_dict1[line_no]
+    str_lines2 = text_dict2[line_no]
+
+    res = cur_str = ""
+    res_line1 = cur_line1 = []
+    res_line2 = cur_line2 = []
 
     while i != 0 and j != 0:
         if(str1[i-1] == str2[j-1]):
             cur_str = cur_str + str1[i-1]
+            cur_line1.append(str_lines1[i-1])
+            cur_line2.append(str_lines2[j-1])
             i = i-1
             j = j-1
 
@@ -54,8 +66,11 @@ def LCS_string(c, str1, str2, min_length):
 
             if len(cur_str) >= threshold:
                 res = res + cur_str
+                res_line1.extend(cur_line1)
+                res_line2.extend(cur_line2)
             
             cur_str = ""
+            cur_line1 = cur_line2 = []
 
             if c[i][j-1] > c[i-1][j]:
                 j = j-1
@@ -63,7 +78,15 @@ def LCS_string(c, str1, str2, min_length):
             else:
                 i = i-1
 
-    return res + cur_str
+
+    res_line1.extend(cur_line1)
+    res_line2.extend(cur_line2)
+
+    return {
+        lines1:utils.unique_elements(res_line1), 
+        lines2:utils.unique_elements(res_line2)
+    }
+    #return res + cur_str
 
 
 if __name__=='__main__':
@@ -76,5 +99,9 @@ if __name__=='__main__':
 
     
     res = LCS(a, b, threshold)
-    print res[::-1]
+
+    print res[lines1]
+
+    print res[lines2]
+    #print res[::-1]
 
