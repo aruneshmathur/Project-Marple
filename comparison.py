92de6041c480b8e538aa96bee1138315c993e8d0
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-02-06 09:15:21 +0530

    Back to LCSubsequence; only gathers those longer than a threshold

--- comparison.py
+++ comparison.py
@@ -4,10 +4,11 @@ import string, utils
 
 text = "TEXT"
 line_no = "LINE_NO"
-threshold = 20
+threshold = 10
 
 
-def longest_common_substring(text_dict1, text_dict2):
+def LCS(text_dict1, text_dict2, min_length):
+    
     if not (text_dict1.has_key(text) and text_dict2.has_key(text)):
         return None
 
@@ -20,24 +21,49 @@ def longest_common_substring(text_dict1, text_dict2):
     m = len(str1)
     n = len(str2)
 
-    result = []
-    x_longest, longest = 0, 0
+    c = utils.get_matrix(m + 1, n + 1)
 
-    c = utils.get_matrix(m, n)
+    res = ""
 
-    for i in range(0, m):
-        for j in range(0, n):
-            if str1[i] == str2[j]:
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i-1] == str2[j-1]:
                 c[i][j] = c[i-1][j-1] + 1
 
-                if c[i][j] > longest:
-                    longest = c[i][j]
-                    x_longest = i
+            else:
+                c[i][j] = max(c[i-1][j], c[i][j-1])
+
+    return LCS_string(c, str1, str2, min_length)
+
+
+def LCS_string(c, str1, str2, min_length):
+
+    i = len(str1) 
+    j = len(str2)
+
+    res = ""
+    cur_str = ""
+
+    while i != 0 and j != 0:
+        if(str1[i-1] == str2[j-1]):
+            cur_str = cur_str + str1[i-1]
+            i = i-1
+            j = j-1
+
+        else:
+
+            if len(cur_str) >= threshold:
+                res = res + cur_str
+            
+            cur_str = ""
+
+            if c[i][j-1] > c[i-1][j]:
+                j = j-1
 
             else:
-                c[i][j] = 0
+                i = i-1
 
-    return str1[x_longest - longest + 1: x_longest + 1]
+    return res + cur_str
 
 
 if __name__=='__main__':
@@ -48,12 +74,7 @@ if __name__=='__main__':
     (a[text], a[line_no]) = utils.file_contents_line_numbers('a.txt')
     (b[text], b[line_no]) = utils.file_contents_line_numbers('b.txt')
 
-    res = longest_common_substring(a, b)
-
-    while len(res) > threshold:
-        print res
-
-        a[text] = string.replace(a[text], res, "")
-        b[text] = string.replace(b[text], res, "")
-        res = longest_common_substring(a, b)
+    
+    res = LCS(a, b, threshold)
+    print res[::-1]
 
