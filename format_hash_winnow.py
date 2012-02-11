85416bcf8675adb7c0c8e47042d07fa885f831d8
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-02-11 17:54:17 +0530

    hack for ignore hash/winnow

--- format_hash_winnow.py
+++ format_hash_winnow.py
@@ -31,7 +31,7 @@ def gen_hash_list(string, file_numbers, k):
         t = (hash_list[i-1][0] - (ord(string[i-1]) * j)) % M
         t = (t * B) % M
         t = (t + ord(string[i+k-1])) % M
-        hash_list.append([t,unique_elements(file_numbers[i:i+k])] )
+        hash_list.append([t,unique_elements(file_numbers[i:i+k])])
 
     return hash_list
 
@@ -50,8 +50,11 @@ def hash_lines(lines_with_no, k = 5):
 def record(hash_list, i):
     return (hash_list[i][0], hash_list[i][1], i)
 
+def record_ignore(hash_list, i):
+    return (hash_list[i], i)
 
-def winnow(hash_list, w = 4):
+
+def winnow(hash_list, w):
 
     winnow_list = []
     hash_list_length = len(hash_list)
@@ -92,4 +95,78 @@ def winnow(hash_list, w = 4):
 
 
     return winnow_list
+
+
+def hash_ignore_lines(string, k):
+    hash_list = []
+
+    length = len(string)
+    count = length - k + 1
+
+    if count <= 0:
+        return None 
+
+    t = 0
+    j = 1
+
+    #Calculate ((B ^ k) - 1) once
+    for i in range(0, k - 1):
+        j = (j * B) % M
+
+    #The initial hash
+    for i in range(0, k):
+        t = ((t * B) + ord(string[i])) % M
+
+
+    hash_list.append(t)
+
+    for i in range(1, count):
+        t = (hash_list[i-1] - (ord(string[i-1]) * j)) % M
+        t = (t * B) % M
+        t = (t + ord(string[i+k-1])) % M
+        hash_list.append(t)
+
+    return hash_list
+
+
+def winnow_ignore(hash_list, w):
+
+    winnow_list = []
+    hash_list_length = len(hash_list)
+
+    param = hash_list_length
+
+    if not param < w:
+        param = w
+
+    global_min = M + 1 
+        
+    for i in range(param - 1, -1, -1):
+        if hash_list[i] < global_min:
+            global_min = hash_list[i]
+            index = i
+
+    winnow_list.append(global_min)
+
+    if hash_list_length < w:
+        return winnow_list
+
+    for i in range(1, hash_list_length - w + 1):
+        if i - index > 0:
+            global_min = M + 1
+
+            for j in range(w + i - 1, i - 1, -1):
+                if hash_list[j] < global_min:
+                    (global_min, index) = record_ignore(hash_list, j)
+
+            winnow_list.append(global_min)
+
+        else:
+            if hash_list[w + i - 1] < global_min:
+                (global_min, index) = record_ignore(hash_list, w + i - 1)
+                winnow_list.append(global_min)
+
+
+    return winnow_list
+
 
